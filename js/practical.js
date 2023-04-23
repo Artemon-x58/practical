62a9a6087db300f609e7fb4c5bb1381175dcd61f
Artemon-x58 <artem1145819@gmail.com>
2023-04-23 14:40:06 +0200

    4

--- js/practical.js
+++ js/practical.js
@@ -1,7 +1,40 @@
 // ЗАДАЧА 1
 // НАПИШІТЬ РЕЧЕННЯ ЗА ДОПОМОГОЮ ШАБЛОННОГО РЯДКА "ДОБРОГО ДНЯ,МЕНЕ ЗВАТИ ГАЛИНА, МЕНІ 35 РОКІВ, Я ВИВЧАЮ JAVASCRIPT"
-const name = 'ГАЛИНА';
-const age = 35;
-const language = 'JAVASCRIPT';
-const hello = `ДОБРОГО ДНЯ, МЕНЕ ЗВАТИ ${name}, МЕНІ ${age} РОКІВ, Я ВИВЧАЮ ${language}`;
-console.log(hello);
+// const name = 'ГАЛИНА';
+// const age = 35;
+// const language = 'JAVASCRIPT';
+// const hello = `ДОБРОГО ДНЯ, МЕНЕ ЗВАТИ ${name}, МЕНІ ${age} РОКІВ, Я ВИВЧАЮ ${language}`;
+// console.log(hello);
+
+
+// ЗАДАЧА 2
+// ЗАПИТАТИ У КОРИСТУВАЧА ІМЯ, ПОЧТУ , ТЕЛЕФОН, І ПОКАЗАТИ У КОНСОЛІ "КОРИСТУВАЧ __ ВИКОРИСТОВУЄ  __ ПОЧТУ І __ ТЕЛЕФОН"
+
+// const name = prompt('What is your name?');
+// const email = prompt('What is your email?');
+// const phone = prompt('What is your phone number?');
+// const message = `КОРИСТУВАЧ ${name} ВИКОРИСТОВУЄ ПОЧТУ ${email} І ТЕЛЕФОН ${phone} `;
+// console.log(message);
+
+// ЗАДАЧА 3
+// ПОРАХУЙТЕ СУМУ ТОВАРІВ У КОРЗИНІ . ПОТРІБНО ВВЕСТИ СУМУ 3-Х ТОВАРІВ І ПОРАХУВАТИ ЇХ СУМУ
+
+// const firstware = Number(prompt('Первый товар'));
+// const secondware = +prompt('Второй товар');
+// const thirdware = Number(prompt('Третий товар'));
+
+// console.log(firstware + secondware + thirdware);
+
+// ЗАДАЧА 4
+// ВИКОРИСТОВУЮЧИ КОНСТРУКЦІЮ IF...ELSE , НАПИШИ КОД, ЯКИЙ БУДЕ ЗАПИТУВАТИ: 
+// "СКІЛЬКИ МІСЯЦІВ МАЄ РІК", ЯКЩО КОРИСТУВАЧ ВВОДИТЬ 12 ТО У КОНСОЛІ ПИШЕ ВІРНО, ЯКЩО НЕ ПРАВИЛЬНО КОРИСТУВАЧ ВВІВ, ТОДІ У КОНСОЛІ ПИШЕ 
+// "НЕ ЗНАЄТЕ? 12"
+
+const month = Number(prompt('СКІЛЬКИ МІСЯЦІВ МАЄ РІК?'));
+if (month === 12){
+    console.log('ВІРНО');
+   
+
+}  else {
+    console.log('НЕ ЗНАЄТЕ? 12');
+}
