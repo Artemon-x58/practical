035ea59f36c6fe5d0c9f5bd3d0f8049f3c654618
xxx3105 <123968288+xxx3105@users.noreply.github.com>
2023-04-23 14:09:44 +0200

    ver3

--- js/practical.js
+++ js/practical.js
@@ -1,7 +1,27 @@
-// ЗАДАЧА 1
-// НАПИШІТЬ РЕЧЕННЯ ЗА ДОПОМОГОЮ ШАБЛОННОГО РЯДКА "ДОБРОГО ДНЯ,МЕНЕ ЗВАТИ ГАЛИНА, МЕНІ 35 РОКІВ, Я ВИВЧАЮ JAVASCRIPT"
-const name = 'ГАЛИНА';
-const age = 35;
-const language = 'JAVASCRIPT';
-const hello = `ДОБРОГО ДНЯ, МЕНЕ ЗВАТИ ${name}, МЕНІ ${age} РОКІВ, Я ВИВЧАЮ ${language}`;
-console.log(hello);
+// // ЗАДАЧА 1
+// // НАПИШІТЬ РЕЧЕННЯ ЗА ДОПОМОГОЮ ШАБЛОННОГО РЯДКА "ДОБРОГО ДНЯ,МЕНЕ ЗВАТИ ГАЛИНА, МЕНІ 35 РОКІВ, Я ВИВЧАЮ JAVASCRIPT"
+// const name = 'ГАЛИНА';
+// const age = 35;
+// const language = 'JAVASCRIPT';
+// const hello = `ДОБРОГО ДНЯ, МЕНЕ ЗВАТИ ${name}, МЕНІ ${age} РОКІВ, Я ВИВЧАЮ ${language}`;
+// console.log(hello);
+
+// ЗАДАЧА 2
+// ЗАПИТАТИ У КОРИСТУВАЧА ІМЯ, ПОЧТУ , ТЕЛЕФОН, І ПОКАЗАТИ У КОНСОЛІ "КОРИСТУВАЧ __ ВИКОРИСТОВУЄ  __ ПОЧТУ І __ ТЕЛЕФОН"
+
+// const nameUser = prompt('укажи имя');
+// const emailUser = prompt('электронная почта');
+// const phoneUser = prompt('телефон');
+
+// console.log (`КОРИСТУВАЧ ${nameUser} ВИКОРИСТОВУЄ  ${emailUser}  ПОЧТУ І ${phoneUser}  ТЕЛЕФОН`)
+
+
+// ЗАДАЧА 3
+// ПОРАХУЙТЕ СУМУ ТОВАРІВ У КОРЗИНІ . ПОТРІБНО ВВЕСТИ СУМУ 3-Х ТОВАРІВ І ПОРАХУВАТИ ЇХ СУМУ
+
+const firstWare = Number(prompt(`Первый товар`));
+const secondWare = +prompt(`Второй товар`);
+const thirdWare = Number(prompt(`Третий товар`));
+
+console.log(firstWare + secondWare + thirdWare)
+
