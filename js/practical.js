3bb345c1e117aeb29c56fe1911d9215e3d12bcd8
VictoriaMendas <124498217+VictoriaMendas@users.noreply.github.com>
2023-04-23 14:03:02 +0200

    #2

--- js/practical.js
+++ js/practical.js
@@ -1,7 +1,14 @@
-// ЗАДАЧА 1
-// НАПИШІТЬ РЕЧЕННЯ ЗА ДОПОМОГОЮ ШАБЛОННОГО РЯДКА "ДОБРОГО ДНЯ,МЕНЕ ЗВАТИ ГАЛИНА, МЕНІ 35 РОКІВ, Я ВИВЧАЮ JAVASCRIPT"
-const name = 'ГАЛИНА';
-const age = 35;
-const language = 'JAVASCRIPT';
-const hello = `ДОБРОГО ДНЯ, МЕНЕ ЗВАТИ ${name}, МЕНІ ${age} РОКІВ, Я ВИВЧАЮ ${language}`;
-console.log(hello);
+// // ЗАДАЧА 1
+// // НАПИШІТЬ РЕЧЕННЯ ЗА ДОПОМОГОЮ ШАБЛОННОГО РЯДКА "ДОБРОГО ДНЯ,МЕНЕ ЗВАТИ ГАЛИНА, МЕНІ 35 РОКІВ, Я ВИВЧАЮ JAVASCRIPT"
+// const name = 'ГАЛИНА';
+// const age = 35;
+// const language = 'JAVASCRIPT';
+// const hello = `ДОБРОГО ДНЯ, МЕНЕ ЗВАТИ ${name}, МЕНІ ${age} РОКІВ, Я ВИВЧАЮ ${language}`;
+// console.log(hello);ЗАДАЧА 2
+// ЗАПИТАТИ У КОРИСТУВАЧА ІМЯ, ПОЧТУ , ТЕЛЕФОН,
+//  І ПОКАЗАТИ У КОНСОЛІ "КОРИСТУВАЧ __ ВИКОРИСТОВУЄ  __ ПОЧТУ І __ ТЕЛЕФОН"
+const name = prompt("What  is your name?");
+const email = prompt("What  is your email?");
+const phone = prompt ("What  is your phone number?");
+const message = `USER ${name} USE ${email} AND ${phone} PHONE`
+console.log(message);
