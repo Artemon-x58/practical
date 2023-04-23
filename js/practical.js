0adac056861b42b16e1e5f7aac565b97b18d5470
Artemon-x58 <artem1145819@gmail.com>
2023-04-23 13:48:35 +0200

    number 1

---
+++ js/practical.js
@@ -0,0 +1,7 @@
+// ЗАДАЧА 1
+// НАПИШІТЬ РЕЧЕННЯ ЗА ДОПОМОГОЮ ШАБЛОННОГО РЯДКА "ДОБРОГО ДНЯ,МЕНЕ ЗВАТИ ГАЛИНА, МЕНІ 35 РОКІВ, Я ВИВЧАЮ JAVASCRIPT"
+const name = 'ГАЛИНА';
+const age = 35;
+const language = 'JAVASCRIPT';
+const hello = `ДОБРОГО ДНЯ, МЕНЕ ЗВАТИ ${name}, МЕНІ ${age} РОКІВ, Я ВИВЧАЮ ${language}`;
+console.log(hello);
