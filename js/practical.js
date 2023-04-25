3f31284efe0a0f4b5c1fc02ff1a3607148dd1006
Demian <demyan.korotchenko@gmail.com>
2023-04-25 07:23:27 +0200

    Task_6 add

--- js/practical.js
+++ js/practical.js
@@ -68,6 +68,54 @@
 //     console.log ("Такого місяця не існує")
 // }
  
+// Задача 6_'через введення повної назви місяця'
+
+const monthBirth = prompt("В якому місяці ви народилися?").toLowerCase();
+console.log(monthBirth);
+let season;
+
+switch (monthBirth) {
+    case 'december':
+        season = 'Winter';
+        break;
+    case 'january':
+        season = 'Winter';
+        break;
+    case 'february':
+        season = 'Winter';
+        break;
+    case 'march':
+        season = 'Spring';
+        break;
+    case 'april':
+        season = 'Spring';
+        break;
+    case 'may':
+        season = 'Spring';
+        break;
+    case 'june':
+        season = 'Summer';
+        break;
+    case 'july':
+        season = 'Summer';
+        break;
+    case 'august':
+        season = 'Summer';
+        break;
+    case 'september':
+        season = 'Autumn';
+        break;
+    case 'october':
+        season = 'Autumn';
+        break;
+    case 'november':
+        season = 'Autumn';
+        break;
+    default:
+        console.log('Такого місяця не існує');
+        break;
+}
+console.log(season);
 
 // ЗАДАЧА 7
 // НАПИШІТЬ ПРОГРАМУ, ЯКА ПРОВІРЯЄ ЛОГІН І ПАРОЛЬ,
