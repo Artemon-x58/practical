90785a76e67b41e2625e2c7eeb20396977d3362e
Ivan Doboni <ivan.doboni3006@gmail.com>
2023-05-07 16:24:30 +0300

    #7

--- js/lesson_2.js
+++ js/lesson_2.js
@@ -73,15 +73,55 @@
 // Напишіть ф-цію calculateAverage() яка приймає довільну кількість аргументів і
 //  повертає їх середнє значення .Всі аргументи будуть тільки числовими
 
-console.log(calculateAverage(1, 2, 3, 4)); // 2.5
-console.log(calculateAverage(14, 8, 2)); // 8
-console.log(calculateAverage(27, 43, 2, 8, 36)); // 23.2
-function calculateAverage() {
-  console.log(arguments);
-  const numbers = Array.from(arguments);
-  let total = 0;
-  for (let number of numbers) {
-    total += number;
-  }
-  return total / numbers.length;
-}
+// console.log(calculateAverage(1, 2, 3, 4)); // 2.5
+// console.log(calculateAverage(14, 8, 2)); // 8
+// console.log(calculateAverage(27, 43, 2, 8, 36)); // 23.2
+// function calculateAverage() {
+//   console.log(arguments);
+//   const numbers = Array.from(arguments);
+//   let total = 0;
+//   for (let number of numbers) {
+//     total += number;
+//   }
+//   return total / numbers.length;
+// }
+
+
+////////////////////////
+// ЗАДАЧА 6
+// Напишіть ф-цію greet(name) , яка при виклику буде получати імя (як приклад , "Василь"), і логінувати рядок "Привіт, <імя>".У випадку , відсутнього 
+// відсутнього значення, виводе аргумент "Привіт , гість"
+// console.log(greet("Василь"));//Привіт Василь
+// console.log(greet());//Привіт гість
+
+// function greet(name = "гість"){
+// 	return `Привіт ${name}`
+// }
+
+//////////////
+// ЗАДАЧА 7
+// Виконайте рефакторинг замінивши обявлення ф-ції з function declaration на function expression та на стрілочну функцію
+// function checkNumbers(a, b) {
+//   if (a > b) {
+//     return `число ${a} більше ${b}`;
+//   }
+
+//   return `число ${b} більше ${a}`;
+// }
+// const checkNumbers = function (a,b) {
+// 	if (a > b) {
+// 		return `число ${a} більше ${b}`;
+// 	  }
+	
+// 	  return `число ${b} більше ${a}`;
+// }
+// const checkNumbers = (a,b) => {
+// 	if (a > b) {
+// 		return `число ${a} більше ${b}`;
+// 	  }
+	
+// 	  return `число ${b} більше ${a}`;
+// }
+
+// const checkNumbers = (a,b) => a > b ? `число ${a} більше ${b}`: `число ${b} більше ${a}`;
+// console.log(checkNumbers(10,30)); 
