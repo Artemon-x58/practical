c9fa31da185b59310941580f8decf97ae8af1d98
Nyekrasov <pnekrasov1910@gmail.com>
2023-05-21 17:16:30 +0300

    last lessons

--- js/lesson3.js
+++ js/lesson3.js
@@ -43,93 +43,93 @@
 7)визначити, чи усі вчені працювали у 19ст;
 */
 
-const scientists = [
-    {
-      name: "Albert",
-      surname: "Einstein",
-      born: 1879,
-      dead: 1955,
-      id: 1,
-    },
-    {
-      name: "Isaac",
-      surname: "Newton",
-      born: 1643,
-      dead: 1727,
-      id: 2,
-    },
-    {
-      name: "Galileo",
-      surname: "Galilei",
-      born: 1564,
-      dead: 1642,
-      id: 3,
-    },
-    {
-      name: "Marie",
-      surname: "Curie",
-      born: 1867,
-      dead: 1934,
-      id: 4,
-    },
-    {
-      name: "Johannes",
-      surname: "Kepler",
-      born: 1571,
-      dead: 1630,
-      id: 5,
-    },
-    {
-      name: "Nicolaus",
-      surname: "Copernicus",
-      born: 1473,
-      dead: 1543,
-      id: 6,
-    },
-    {
-      name: "Max",
-      surname: "Planck",
-      born: 1858,
-      dead: 1947,
-      id: 7,
-    },
-    {
-      name: "Katherine",
-      surname: "Blodgett",
-      born: 1898,
-      dead: 1979,
-      id: 8,
-    },
-    {
-      name: "Ada",
-      surname: "Lovelace",
-      born: 1815,
-      dead: 1852,
-      id: 9,
-    },
+// const scientists = [
+//     {
+//       name: "Albert",
+//       surname: "Einstein",
+//       born: 1879,
+//       dead: 1955,
+//       id: 1,
+//     },
+//     {
+//       name: "Isaac",
+//       surname: "Newton",
+//       born: 1643,
+//       dead: 1727,
+//       id: 2,
+//     },
+//     {
+//       name: "Galileo",
+//       surname: "Galilei",
+//       born: 1564,
+//       dead: 1642,
+//       id: 3,
+//     },
+//     {
+//       name: "Marie",
+//       surname: "Curie",
+//       born: 1867,
+//       dead: 1934,
+//       id: 4,
+//     },
+//     {
+//       name: "Johannes",
+//       surname: "Kepler",
+//       born: 1571,
+//       dead: 1630,
+//       id: 5,
+//     },
+//     {
+//       name: "Nicolaus",
+//       surname: "Copernicus",
+//       born: 1473,
+//       dead: 1543,
+//       id: 6,
+//     },
+//     {
+//       name: "Max",
+//       surname: "Planck",
+//       born: 1858,
+//       dead: 1947,
+//       id: 7,
+//     },
+//     {
+//       name: "Katherine",
+//       surname: "Blodgett",
+//       born: 1898,
+//       dead: 1979,
+//       id: 8,
+//     },
+//     {
+//       name: "Ada",
+//       surname: "Lovelace",
+//       born: 1815,
+//       dead: 1852,
+//       id: 9,
+//     },
   
-    {
-      name: "Lise",
-      surname: "Meitner",
-      born: 1878,
-      dead: 1968,
-      id: 11,
-    },
-    {
-      name: "Sarah E.",
-      surname: "Goode",
-      born: 1855,
-      dead: 1905,
-      id: 10,
-    },
-    {
-      name: "Hanna",
-      surname: "Hammarström",
-      born: 1829,
-      dead: 1909,
-      id: 12,
-    },
-  ];
+//     {
+//       name: "Lise",
+//       surname: "Meitner",
+//       born: 1878,
+//       dead: 1968,
+//       id: 11,
+//     },
+//     {
+//       name: "Sarah E.",
+//       surname: "Goode",
+//       born: 1855,
+//       dead: 1905,
+//       id: 10,
+//     },
+//     {
+//       name: "Hanna",
+//       surname: "Hammarström",
+//       born: 1829,
+//       dead: 1909,
+//       id: 12,
+//     },
+//   ];
 
   // 1)получити масив вчених які народились у 19ст;
 /*
@@ -139,7 +139,7 @@ const scientists = [
         console.log(allScients);
         */
 
-// 2)порахувати загальну суму років скільки прожили вчені , 
+// 2)порахувати загальну суму років скільки прожили вчені ,
 //і визначити середнє значення;
 /*
     const totalYears = scientists.reduce((acc, scientist) => 
@@ -166,7 +166,7 @@ console.log(sortAlph);
 const oldSchoolScientists = scientists.filter(scientist => 
     scientist.born < 1700 && scientist.born > 1400);
     console.log(oldSchoolScientists);
- */  
+ */
     
 //6)знайти рік народження Albert Einstein;
 
@@ -189,3 +189,136 @@ const activeLive = scientists.every(scientist =>
 */
 
 
+// ЗАДАЧА 4
+// Сортування примітивів.
+// Виконати сортування масиву цін зі спадання та зростання.
+// const prices = [2, 14, 1, 37, 26, 8];
+
+// const newPriceUp = [...prices].sort((a, b) => a - b);
+// const newPriceDown = [...prices].sort((a, b) => b - a);
+// console.log(newPriceUp);
+// console.log(newPriceDown);
+
+// ЗАДАЧА 5
+// Виконати сортування масиву назв моніторів в алфавітному та зворотному алфавітному порядку.
+// const items = ["SAMSUNG", "LG", "ASUS", "DELL", "BENQ"];
+
+// const nameUp = [...items].sort((a, b) => a.localeCompare(b));
+// const nameDown = [...items].sort((a, b) => b.localeCompare(a));
+
+// console.log(nameUp);
+// console.log(nameDown);
+
+
+// ЗАДАЧА 6
+// Зібрати в allTopics масив усіх предметів всіх курсів використовуючи flatMap.
+// Використовуючи Array.prototype.filter виконати фільтрацію, залишивши в uniqueTopics тільки унікальні елементи.
+
+// const courses = [
+//   {
+//     name: "Basic HTML+CSS",
+//     topics: ["VSCode", "HTML", "CSS", "GitHub Desktop", "GitHub"],
+//   },
+//   {
+//     name: "Intermediate HTML+CSS",
+//     topics: ["VSCode", "Terminal", "Git", "GitHub", "HTML", "CSS"],
+//   },
+//   {
+//     name: "Basic JavaScript",
+//     topics: [
+//       "VSCode",
+//       "Type system",
+//       "Loops",
+//       "Functions",
+//       "Conditions",
+//       "Classes",
+//       "DOM",
+//       "Git",
+//       "GitHub",
+//     ],
+//   },
+//   {
+//     name: "Intermediate JavaScript",
+//     topics: [
+//       "VSCode",
+//       "NPM",
+//       "Bundlers",
+//       "Transpiling",
+//       "Promises",
+//       "AJAX",
+//       "Git",
+//       "GitHub",
+//     ],
+//   },
+// ];
+
+// const allTopics = courses.flatMap((courses) => courses.topics);
+// console.table(allTopics);
+
+// const unikEl = allTopics.filter((allTopics, index, arrayEl) => arrayEl.indexOf(allTopics) === index);
+// console.table(unikEl);
+
+// ЗАДАЧА 1
+// 1) Напишіть ф - ю конструктор яка створює обєкт person з такимим ключами:
+// const Person = {
+//   firstName,
+//   lastName,
+//   age,
+//   gender,
+//   interest,
+//   bio() {
+//     // виводить рядок Привіт {імя} мені {вік} років. Мені подобається {інтерес}
+//   },
+//   greeting() {
+//     // Привіт, я {імя}
+//   },
+// };
+
+// function Person({ firstName, lastName, age, gender, interest }) {
+  
+//   this.firstName = firstName;
+//   this.lastName = lastName;
+//   this.age = age;
+//   this.gender = gender;
+//   this.interest = interest;
+// Person.prototype.bio = function() {
+//    console.log(`Привіт ${this.firstName} мені ${this.age} років. Мені подобається  ${this.interest}`)
+// }
+  
+//   Person.prototype.greeting = function() {
+//   console.log(`Привіт, я ${this.firstName}`)
+//   }
+ 
+//   }
+
+// const newPerson = new Person({ firstName: 'Pavlo', lastName: "Smit", age: '40', gender: 'men', interest: 'book' });
+
+// console.log(newPerson);
+
+// ЗАДАЧА 2
+// Напишіть клас User для створення користувача з наступними властивостями:
+// username - імя, рядок
+// age - вік, число
+// numberOfPosts - кількість постів, число
+// Клас очікує один параметр - об'єкт налаштувань з однойменними властивостями.
+// Добавте метод getInfo(), який повертає рядок:
+// User ${ імя } is ${ вік } years old and has ${ кількість постів } posts.
+
+class User {
+  constructor({ username, age, numberOfPosts }) { 
+    this.username = username;
+    this.age = age;
+    this.numberOfPosts = numberOfPosts;
+
+  }
+  getInfo() { 
+    return `User ${this.username} is ${this.age} years old and has ${this.numberOfPosts} posts.`
+  }
+
+getInfo2 = () => { 
+    return `User ${this.username} is ${this.age} years old and has ${this.numberOfPosts} posts.`
+  }
+
+}
+const newUser = new User({ username: 'Pavel', age: 25, numberOfPosts: 25 });
+console.log(newUser);
