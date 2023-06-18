673e1c634e704e46dc107d7a08f2b7ba4fd4806d
xxx3105 <123968288+xxx3105@users.noreply.github.com>
2023-06-18 15:14:57 +0200

    18/06/23 task1

---
+++ js/lesson5.js
@@ -0,0 +1,47 @@
+// 3) УМОВА
+// Зробити фіксований скрол на сторінці для менюшки, тобто,
+//     щоб наше меню фіксувалось зверху браузера при скролі,
+//     для того потрібно порахувати висоту хедера,
+//         відстежуємо позицію скролу та
+// додавати клас fixed - nav якщо скрол більше висоту
+// хедера, в іншому випадку його видаляємо
+
+
+const nav = document.getElementById('navigation');
+const navHeader = document.querySelector('header');
+
+
+
+// pageYOffset
+
+// scrollY
+
+    
+const dataWidth = navHeader.clientWidth;
+const dataHeight = navHeader.clientHeight;
+
+
+function scrollControl() {
+
+    if (scrollY > dataHeight) {
+    nav.classList.add("fixed-nav")
+    }
+    else { 
+    nav.classList.remove("fixed-nav")
+    }
+    
+}
+
+window.addEventListener('scroll', scrollControl)
+
+
+
+
+
+
+
+
+
+
+
+
